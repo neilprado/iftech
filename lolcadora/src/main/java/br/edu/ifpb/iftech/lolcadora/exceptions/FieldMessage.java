a6fc69345d456feb7cabbb6bdb21fd0e3a969af6
neilprado <neiljohn17@gmail.com>
2019-11-13 16:36:54 -0300

    Classes de exceção criadas

---
+++ lolcadora/src/main/java/br/edu/ifpb/iftech/lolcadora/exceptions/FieldMessage.java
@@ -0,0 +1,22 @@
+package br.edu.ifpb.iftech.lolcadora.exceptions;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class FieldMessage implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    private String fieldName;
+    private String message;
+
+
+    public FieldMessage() {
+    }
+
+    public FieldMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+}
