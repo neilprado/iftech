301a1aefd37c46f66d1b60dceb10eec7fa71a21e
neilprado <neiljohn17@gmail.com>
2019-11-18 23:10:44 -0300

    Order Request and Response done

--- lolcadora/src/main/java/br/edu/ifpb/iftech/lolcadora/dto/request/OrderRequest.java
+++ lolcadora/src/main/java/br/edu/ifpb/iftech/lolcadora/dto/request/OrderRequest.java
@@ -1,8 +1,16 @@
 package br.edu.ifpb.iftech.lolcadora.dto.request;
 
+import br.edu.ifpb.iftech.lolcadora.model.OrderItem;
+import br.edu.ifpb.iftech.lolcadora.model.User;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 public class OrderRequest {
+    private LocalDate dataPedido;
+    private User userLogin;
+    private Set<OrderItem> itens;
 
 }
--- lolcadora/src/main/java/br/edu/ifpb/iftech/lolcadora/dto/response/OrderResponse.java
+++ lolcadora/src/main/java/br/edu/ifpb/iftech/lolcadora/dto/response/OrderResponse.java
@@ -1,4 +1,41 @@
 package br.edu.ifpb.iftech.lolcadora.dto.response;
 
+import br.edu.ifpb.iftech.lolcadora.model.Order;
+import br.edu.ifpb.iftech.lolcadora.model.OrderItem;
+import br.edu.ifpb.iftech.lolcadora.model.User;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
 public class OrderResponse {
+    private Long id;
+    private LocalDate dataPedido;
+    private User user;
+    private Set<OrderItem> itens;
+
+    public static OrderResponse from(Order order){
+       OrderResponse orderResponse = new OrderResponse();
+       orderResponse.setId(order.getId());
+       orderResponse.setDataPedido(order.getDataPedido());
+       orderResponse.setItens(order.getItens());
+       orderResponse.setUser(order.getUser());
+       return orderResponse;
+    }
+
+    public static Page<OrderResponse> from(Page<Order> orders){
+        Page<OrderResponse> orderResponses = orders.map(order -> {
+
+            OrderResponse orderResponse = new OrderResponse();
+
+            orderResponse.setId(order.getId());
+            orderResponse.setDataPedido(order.getDataPedido());
+            orderResponse.setItens(order.getItens());
+            orderResponse.setUser(order.getUser());
+            return orderResponse;
+        });
+        return orderResponses;
+    }
 }
